7cc784c27d5e54ad7acafda3e61a4afbfdebcf60
jaelleoupoh23-jaja <jaelle.oupoh23@gmail.com>
2026-06-22 13:56:17 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -521,7 +521,7 @@ const points = valeur === 6 ? 1.5 : valeur
             ))}
           </div>
 
-          <div style={st.robotBox}>   🤖 {messageTour ? messageTour : 'À moi la victoire ! Je ne plaisante pas 😄'} </div>
+          <div style={st.zoneDe}>   <div style={st.robotBox}>     🤖 {messageTour ? messageTour : 'À moi la victoire ! Je ne plaisante pas 😄'}   </div>
             <FaceDe valeur={valeurAffichee} enTrain={enTrainDeLancer} />
             {dernierLancer && !enTrainDeLancer && (
               <div style={{ fontSize: 13, color: '#9a93b5', marginTop: 12 }}>
