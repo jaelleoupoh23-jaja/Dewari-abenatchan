d5bb62809a64072f5b8d37b14635a3092ad198e1
jaelleoupoh23-jaja <jaelle.oupoh23@gmail.com>
2026-06-26 23:06:30 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2210,4 +2210,3 @@ jungleDecoration: {
   boutonEnvoyer: { background: 'linear-gradient(135deg,#FF4D6D,#7B2CBF)', color: '#fff', border: 'none', borderRadius: '50%', width: 36, height: 36, fontSize: 16, flexShrink: 0 },
 }
 }
-}
