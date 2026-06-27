d6243b4cb45a1e3de311dc15ffa92dad30908fa4
jaelleoupoh23-jaja <jaelle.oupoh23@gmail.com>
2026-06-27 17:26:10 +0000

    Fix formatting in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -442,7 +442,6 @@ function Accueil({ salons, tournoi, inscritTournoi, onChoisirSalon, onOuvrirTour
 
 
 <div
- 
       <div ref={refSalons} style={st.section}>
         <div style={st.sectionTitre}>Choisis ton salon</div>
         <div style={st.sectionSousTitre}>20 joueurs max par salon</div>
